1f2740e9a243aa9e185429249d14cef2e95bcbab
Georgi <georgichichev15@Gmail.com>
2022-09-21 13:48:00 +0300

    Move planets to separate file

---
+++ src/planets.js
@@ -0,0 +1,10 @@
+export const planets = [
+    {name: 'mercury', x: 500},
+    {name: 'venus', x: 350},
+    {name: 'earth', x: 200},
+    {name: 'mars', x: 50},
+    {name: 'jupiter', x: -100},
+    {name: 'uranus', x: -225},
+    {name: 'neptune', x: -370},
+    {name: 'pluto', x: -500},
+];
