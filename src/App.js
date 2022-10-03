9fc161d2282fddf8a7cac618206d19a62fca47f2
Georgi <georgichichev15@Gmail.com>
2022-10-03 11:59:18 +0300

    Remove react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-import { BrowserRouter } from "react-router-dom";
 import Planets from "./components/Planets/Planets.js";
 
 function App() {
     return (
-        <BrowserRouter>
-            <div>
-                <Planets/>
-            </div>
-        </BrowserRouter>
+        <Planets/>
     );
 }
 
